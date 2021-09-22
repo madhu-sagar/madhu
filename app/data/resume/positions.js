b9eaee22190c2c1f238abb6dd3735b86d85cb149
Madhu Sagar <48498520+madhu-sagar@users.noreply.github.com>
2021-09-23 01:00:36 +0530

    Update

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -13,10 +13,17 @@ const positions = [
     company: 'Anivale Pvt Ltd',
     position: 'Software Developer',
     link: '',
-    daterange: 'June 2018-Present',
+    daterange: 'June 2018-Aug 2020',
     points: [
       'Web Development in the modern tech stack'
       ],
+  },
+  {
+    company : 'Robosoft Technologies',
+    position : 'Software Engineer',
+    link : '',
+    daterange : 'Sep 2020-Present'.
+    points : ['Working in React,Typescript,Node.js stack]
   }
 ];
 
