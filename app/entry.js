73fd8e41f97a320dc167148a346d1de822095b0b
madhu-sagar <48498520+madhu-sagar@users.noreply.github.com>
2026-03-01 01:39:34 +0000

    feat : lazy loading

--- app/entry.js
+++ app/entry.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 
-// Featured
+// Eagerly loaded
 import Index from './views/Index';
-import About from './views/About';
-import Projects from './views/Projects';
-import Resume from './views/Resume';
-import Stats from './views/Stats';
-import Contact from './views/Contact';
-import Blog from './views/Blog';
-import BlogPost from './views/BlogPost';
-
-import NotFound from './views/NotFound';
 
 // All of our CSS
 import './static/css/main.scss';
 
+// Lazy loaded
+const About = lazy(() => import('./views/About'));
+const Projects = lazy(() => import('./views/Projects'));
+const Resume = lazy(() => import('./views/Resume'));
+const Stats = lazy(() => import('./views/Stats'));
+const Contact = lazy(() => import('./views/Contact'));
+const Blog = lazy(() => import('./views/Blog'));
+const BlogPost = lazy(() => import('./views/BlogPost'));
+const NotFound = lazy(() => import('./views/NotFound'));
+
 ReactDOM.render(
   <Router basename={BASE_PATH}>
-    <Switch>
-      <Route exact path="/" component={Index} />
-      <Route path="/about" component={About} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/stats" component={Stats} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/resume" component={Resume} />
-      <Route exact path="/blog" component={Blog} />
-      <Route path="/blog/:slug" component={BlogPost} />
-      {/* Only useful in development mode */}
-      <Route component={NotFound} status={404} />
-    </Switch>
+    <Suspense fallback={<div />}>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/blog/:slug" component={BlogPost} />
+        {/* Only useful in development mode */}
+        <Route component={NotFound} status={404} />
+      </Switch>
+    </Suspense>
   </Router>,
   document.getElementById('root'),
 );
